353bf1855f5fa8c15b13f63f61c27a25524f0b7d
QianJin2013 <osexp2003@gmail.com>
2014-07-24 23:32:55 +0900

    define CC=gcc explicitly

--- src/3_build_libx264_armv5.sh
+++ src/3_build_libx264_armv5.sh
@@ -5,15 +5,16 @@ cd ./ffmpeg_src/libx264_src || { echo please download libx264 source to [./ffmpe
 
 ln -sf `which perl` . #link system's perl to current dir
 
+SYS_ROOT="$ANDROID_NDK_ROOT/platforms/android-8/arch-arm"
 TOOL_CHAIN_DIR=`ls -d $ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.*/prebuilt/* | tail -n 1` || exit 1
 LIBGCC_DIR=`ls -d $TOOL_CHAIN_DIR/lib/gcc/arm-linux-androideabi/4.* | tail -n 1` || exit 1
 LIBEXEC_DIR=`ls -d $TOOL_CHAIN_DIR/libexec/gcc/arm-linux-androideabi/4.* | tail -n 1` || exit 1
-SYS_ROOT="$ANDROID_NDK_ROOT/platforms/android-8/arch-arm"
 CPP_ROOT=`ls -d $ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/4.* | tail -n 1` || exit 1
 MAKE_DIR=`ls -d $ANDROID_NDK_ROOT/prebuilt/*/bin | tail -n 1` || exit 1
 export  CFLAGS="-O3 --sysroot=$SYS_ROOT -I$SYS_ROOT/usr/include -I$LIBGCC_DIR/include -I$CPP_ROOT/include"
 export LDFLAGS="-B$SYS_ROOT/usr/lib -B$LIBGCC_DIR -B$TOOL_CHAIN_DIR/arm-linux-androideabi/bin -B$LIBEXEC_DIR -B$CPP_ROOT/libs/armeabi"
 export PATH="$TOOL_CHAIN_DIR/arm-linux-androideabi/bin:$LIBEXEC_DIR:$MAKE_DIR:$PATH"
+export CC=gcc
 
 echo ---------------config libx264 [armv5]--------------------
 ./configure --host=armv5-linux-androideabi --enable-static --disable-cli --enable-pic --prefix=./qj_armv5 \
