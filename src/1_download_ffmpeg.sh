ec61d8f017de896ca3c4a6aedbb0d79306693978
QianJin2013 <osexp2003@gmail.com>
2014-10-16 13:34:17 +0900

    upgrade to ffmpeg 2.4

--- src/1_download_ffmpeg.sh
+++ src/1_download_ffmpeg.sh
@@ -2,8 +2,8 @@
 
 echo ---------------download ffmpeg--------------------
 ls -d ffmpeg_src && { echo ffmpeg_src already exist; exit 0; }
-ls ffmpeg-2.1.3.tar.bz2 || { wget http://www.ffmpeg.org/releases/ffmpeg-2.1.3.tar.bz2 || exit 1; }
-tar -xvjf ffmpeg-2.1.3.tar.bz2 || exit 1
-mv ffmpeg-2.1.3 ffmpeg_src || exit 1
+ls ffmpeg-2.4.tar.bz2 || { wget http://www.ffmpeg.org/releases/ffmpeg-2.4.tar.bz2 || exit 1; }
+tar -xvjf ffmpeg-2.4.tar.bz2 || exit 1
+mv ffmpeg-2.4 ffmpeg_src || exit 1
 
 echo ""; echo ok; echo ""
